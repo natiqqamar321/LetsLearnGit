412707975897796efbbd6164d43530d398baf6d7
Natiq Qamar <natiqqamar321@gmail.com>
2023-01-17 13:43:32 +0530

    adding modified sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ pulic class sum{
     public static void main(String[] args){
         int a=5;
         int b=10;
-        System.out.println(a+b);
+        System.out.println("Sum of a and b"+a+b);
     }
 }
